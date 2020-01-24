696913e4f2b9401960aced6a5b27ac8740cb70fb
Pixel Penguins <info@pixelpenguins.com>
2020-01-24 17:59:48 +0100

    MCOMIX papirus icons updated

--- papirus-patched/papirus-mcomix/install.sh
+++ papirus-patched/papirus-mcomix/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cp -R images images-dup
-su root -c "cp -rf ./images-dup/* /usr/lib/python2.7/dist-packages/mcomix/images/ && echo OK && exit"
+su root -c "cp -rf ./images-dup/* /usr/lib/python2.7/dist-packages/mcomix/images/ && cp -rf ./images-dup/*  /usr/share/mcomix/mcomix/images/ && echo OK && exit"
 rm images-dup -rf
-read $tecla
+read tecla
 
 
