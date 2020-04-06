815f8b71fec4f82e9b8d2ce85d6e14fe1aef3392
Pixel Penguins <info@pixelpenguins.com>
2020-04-06 20:03:36 +0200

    Papirus new release!

---
+++ papirus-patched/papirus-zim/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp -R pixmaps pixmaps-dup
+su root -c "mv -f ./pixmaps-dup/* /usr/share/zim/pixmaps/ && echo OK && exit"
+rm pixmaps-dup -rf
+read $tecla
+
+
--- papirus-patched/papitus-zim/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cp -R pixmaps pixmaps-dup
-su root -c "mv -f ./pixmaps-dup/* /usr/share/zim/pixmaps/ && echo OK && exit"
-rm pixmaps-dup -rf
-read $tecla
-
-
